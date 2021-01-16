b9c09d163555c6fb4a685be4745dbbc2a5d9f35b
natee <natee.natea@gmail.com>
2021-01-17 01:56:36 +0700

    add access token

--- webhook.php
+++ webhook.php
@@ -1,7 +1,7 @@
 <?php
 
     include('server.php'); 
-    
+
     $LINEData = file_get_contents('php://input');
     $jsonData = json_decode($LINEData,true);
 
@@ -42,7 +42,7 @@
     }
 
     $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
-    $lineData['AccessToken'] = "(ใส่ Channel AccessToken ของคุณตรงนี้)";
+    $lineData['AccessToken'] = "uEbhTcwlpe54y5BHzyjzFpmp8IjkmYvEftYlagXn2HijGkFNv3ONRMVE72iqX5YJETG1T59BEhq4d9T+2x9Vs5QFyLNytZVsV0zbPEvpV51g7H3j7TmJuFTZ1clOB7PlzPTYE/bCXc3a2NNyRC47nAdB04t89/1O/w1cDnyilFU=";
 
     $replyJson["replyToken"] = $replyToken;
     $replyJson["messages"][0] = $replyText;
