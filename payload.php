a10dfed6d2927f3ae4c1689a1f963a94cfae8a4b
Notebook <natee.natea@gmail.com>
2021-01-18 17:55:39 +0700

    Remove fetch data

--- payload.php
+++ payload.php
@@ -2,142 +2,79 @@
     require_once('server.php');
     session_start();
 
-    $select_stmt = $db->prepare("SELECT * FROM FoodList");
-    $select_stmt->execute();
+    // $select_stmt = $db->prepare("SELECT * FROM FoodList");
+    // $select_stmt->execute();
 
-    // while($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
-    //     $row["id"];
-    //     $row["FoodName"];
-    //     $row["FoodPrice"];
-    //     $row["FoodImage"];
-    // }
-    // $test = "เกี่ยวกับเรา";
-    // "text": "'.$test.'",
+    // // while($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
+    // //     $row["id"];
+    // //     $row["FoodName"];
+    // //     $row["FoodPrice"];
+    // //     $row["FoodImage"];
+    // // }
+    // // $test = "เกี่ยวกับเรา";
+    // // "text": "'.$test.'",
     
-    $MenuArray = [];
-    while($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
-        $MenuArray[] = [
-            {
-                "type": "bubble",
-                "direction": "ltr",
-                "hero": {
-                  "type": "image",
-                  "url": "https://line-chatbot-icute-interns-php.herokuapp.com/upload/'.$row["FoodImage"].'",
-                  "size": "full",
-                  "aspectRatio": "20:13",
-                  "aspectMode": "cover",
-                  "action": {
-                    "type": "uri",
-                    "label": "Action",
-                    "uri": "https://linecorp.com"
-                  }
-                },
-                "body": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "spacing": "md",
-                  "action": {
-                    "type": "uri",
-                    "label": "Action",
-                    "uri": "https://linecorp.com"
-                  },
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "'.$row["FoodName"].'",
-                      "weight": "bold",
-                      "size": "xl",
-                      "contents": []
-                    },
-                    {
-                      "type": "box",
-                      "layout": "baseline",
-                      "margin": "md",
-                      "contents": [
-                        {
-                          "type": "icon",
-                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                          "type": "icon",
-                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                          "type": "icon",
-                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                          "type": "icon",
-                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                        },
-                        {
-                          "type": "icon",
-                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                        },
-                        {
-                          "type": "text",
-                          "text": "4.0",
-                          "size": "sm",
-                          "color": "#999999",
-                          "flex": 0,
-                          "margin": "md",
-                          "contents": []
-                        }
-                      ]
-                    },
-                    {
-                      "type": "box",
-                      "layout": "vertical",
-                      "spacing": "sm",
-                      "contents": [
-                        {
-                          "type": "box",
-                          "layout": "baseline",
-                          "contents": [
-                            {
-                              "type": "text",
-                              "text": "'.$row["FoodPrice"].'",
-                              "weight": "bold",
-                              "margin": "sm",
-                              "contents": []
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                },
-                "footer": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "contents": [
-                    {
-                      "type": "spacer",
-                      "size": "xs"
-                    },
-                    {
-                      "type": "button",
-                      "action": {
-                        "type": "message",
-                        "label": "สั่งอาหาร",
-                        "text": "สั่งอาหาร"
-                      },
-                      "color": "#52E5F4FF",
-                      "style": "primary",
-                      "gravity": "top"
-                    }
-                  ]
-                }
-            },
-        ]
-    }
+    // $MenuArray = [];
+    // while($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
+    //     $MenuArray[] = [
+    //         {
+    //             "type": "bubble",
+    //             "direction": "ltr",
+    //             "header": {
+    //               "type": "box",
+    //               "layout": "vertical",
+    //               "contents": [
+    //                 {
+    //                   "type": "text",
+    //                   "text": "Header",
+    //                   "align": "center",
+    //                   "contents": []
+    //                 }
+    //               ]
+    //             },
+    //             "hero": {
+    //               "type": "image",
+    //               "url": "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
+    //               "size": "full",
+    //               "aspectRatio": "1.51:1",
+    //               "aspectMode": "fit"
+    //             },
+    //             "body": {
+    //               "type": "box",
+    //               "layout": "vertical",
+    //               "contents": [
+    //                 {
+    //                   "type": "text",
+    //                   "text": "'.$row["FoodName"].'",
+    //                   "align": "center",
+    //                   "contents": []
+    //                 }
+    //               ]
+    //             },
+    //             "footer": {
+    //               "type": "box",
+    //               "layout": "horizontal",
+    //               "contents": [
+    //                 {
+    //                   "type": "button",
+    //                   "action": {
+    //                     "type": "uri",
+    //                     "label": "Button",
+    //                     "uri": "https://linecorp.com"
+    //                   }
+    //                 }
+    //               ]
+    //             }
+    //           },
+    //     ]
+    // }
 
-    $getMenu = '{
-        "type": "carousel",
-        "contents": [
-            '.$MenuArray.'
-        ]
-      }';
+    // $getMenu = '{
+    //     "type": "carousel",
+    //     "contents": [
+    //         '.$MenuArray.'
+    //     ]
+    //   }';
 
     $getMe = '{
         "type": "flex",
