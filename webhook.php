31b97744401c42ef0b71524a5062c178e0464a3b
Notebook <natee.natea@gmail.com>
2021-01-18 14:59:23 +0700

    Test flex message

--- webhook.php
+++ webhook.php
@@ -32,73 +32,48 @@
         $replyText["type"] = "text";
         $replyText["text"] = "Hello from Heroku";
     } else if ($text == "เมนูอาหาร") {
-        $replyText = [
-        {
-            "type": "bubble",
-            "direction": "ltr",
-            "header": {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "ติดต่อเรา",
-                  "align": "center",
-                  "contents": []
-                }
-              ]
-            },
-            "hero": {
-              "type": "image",
-              "url": "https://amarinacademy.com/app/uploads/2017/06/petr-sevcovic-594807-unsplash.jpg",
-              "size": "full",
-              "aspectRatio": "1.51:1",
-              "aspectMode": "fit"
-            },
-            "body": {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "เปิดทำการเวลา 10.00 - 18.00 น.",
-                  "align": "center",
-                  "contents": []
-                }
-              ]
-            },
-            "footer": {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "button",
-                  "action": {
-                    "type": "uri",
-                    "label": "Facebook",
-                    "uri": "https://linecorp.com"
+        $replyText = {
+            "type": "flex",
+            "altText": "Flex Message",
+            "contents": {
+              "type": "bubble",
+              "direction": "ltr",
+              "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "ช่องทางการรับอาหาร",
+                    "align": "center",
+                    "contents": []
                   }
-                },
-                {
-                  "type": "button",
-                  "action": {
-                    "type": "uri",
-                    "label": "เบอร์โทร",
-                    "uri": "https://linecorp.com"
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "รับเองที่ร้าน",
+                      "uri": "https://linecorp.com"
+                    }
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "จัดส่งที่บ้าน",
+                      "uri": "https://linecorp.com"
+                    }
                   }
-                },
-                {
-                  "type": "button",
-                  "action": {
-                    "type": "uri",
-                    "label": "แผนที่ร้าน",
-                    "uri": "https://linecorp.com"
-                  }
-                }
-              ]
+                ]
+              }
             }
           }
-        ];
     } else {
         $replyText["type"] = "text";
         $replyText["text"] = $text;
