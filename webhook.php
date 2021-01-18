247c53265bedb0c911a359d4abe7dd85110cc49d
Notebook <natee.natea@gmail.com>
2021-01-18 15:23:27 +0700

    Test flex message

--- webhook.php
+++ webhook.php
@@ -33,68 +33,72 @@
         $replyText["text"] = "Hello from Heroku";
     } else if ($text == "เมนูอาหาร") {
         $JsonFlex = '{
-            "type": "bubble",
-            "direction": "ltr",
-            "header": {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "ติดต่อเรา",
-                  "align": "center",
-                  "contents": []
-                }
-              ]
-            },
-            "hero": {
-              "type": "image",
-              "url": "https://amarinacademy.com/app/uploads/2017/06/petr-sevcovic-594807-unsplash.jpg",
-              "size": "full",
-              "aspectRatio": "1.51:1",
-              "aspectMode": "fit"
-            },
-            "body": {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "เปิดทำการเวลา 10.00 - 18.00 น.",
-                  "align": "center",
-                  "contents": []
-                }
-              ]
-            },
-            "footer": {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "button",
-                  "action": {
-                    "type": "uri",
-                    "label": "Facebook",
-                    "uri": "https://linecorp.com"
+            "type": "flex",
+            "altText": "Flex Message",
+            "contents": {
+              "type": "bubble",
+              "direction": "ltr",
+              "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "ติดต่อเรา",
+                    "align": "center",
+                    "contents": []
                   }
-                },
-                {
-                  "type": "button",
-                  "action": {
-                    "type": "uri",
-                    "label": "เบอร์โทร",
-                    "uri": "https://linecorp.com"
+                ]
+              },
+              "hero": {
+                "type": "image",
+                "url": "https://amarinacademy.com/app/uploads/2017/06/petr-sevcovic-594807-unsplash.jpg",
+                "size": "full",
+                "aspectRatio": "1.51:1",
+                "aspectMode": "fit"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "เปิดทำการเวลา 10.00 - 18.00 น.",
+                    "align": "center",
+                    "contents": []
                   }
-                },
-                {
-                  "type": "button",
-                  "action": {
-                    "type": "uri",
-                    "label": "แผนที่ร้าน",
-                    "uri": "https://linecorp.com"
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Facebook",
+                      "uri": "https://linecorp.com"
+                    }
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "เบอร์โทร",
+                      "uri": "https://linecorp.com"
+                    }
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "แผนที่ร้าน",
+                      "uri": "https://linecorp.com"
+                    }
                   }
-                }
-              ]
+                ]
+              }
             }
           }';
           $replyText = json_decode($JsonFlex);      
