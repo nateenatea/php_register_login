0ee7e8562b6185944e1f21889c8e90f72b77de17
natee <natee.natea@gmail.com>
2021-02-05 21:02:30 +0700

    Test

--- webhook.php
+++ webhook.php
@@ -107,7 +107,12 @@
         $AccessToken = $getAccessTokenNum['uid'];
     }
 
-    echo $AccessToken;
+    if(!empty($AccessToken)) {
+        echo "WORK !!";
+    }
+    else {
+        echo "NOT WORK !!"
+    }
 
     $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
     $lineData['AccessToken'] = "uEbhTcwlpe54y5BHzyjzFpmp8IjkmYvEftYlagXn2HijGkFNv3ONRMVE72iqX5YJETG1T59BEhq4d9T+2x9Vs5QFyLNytZVsV0zbPEvpV51g7H3j7TmJuFTZ1clOB7PlzPTYE/bCXc3a2NNyRC47nAdB04t89/1O/w1cDnyilFU=";
