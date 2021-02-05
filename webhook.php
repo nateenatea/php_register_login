8864b222bfb19dd1774695667d26d9c87736b507
natee <natee.natea@gmail.com>
2021-02-05 21:45:26 +0700

    test

--- webhook.php
+++ webhook.php
@@ -93,11 +93,11 @@
     // }
     // $replyText["text"] = "สวัสดีคุณ $Name $Surname (#$CustomerID)";
 
-    $getAccessToken = $db->prepare("SELECT * FROM `users` WHERE `uid` = $uid");
-    $getAccessToken->execute();
-    while($getAccessTokenNum = $getAccessToken->fetch(PDO::FETCH_ASSOC)) {
-        $AccessToken = $getAccessTokenNum['accesstoken_lineoa'];
-    }
+    // $getAccessToken = $db->prepare("SELECT * FROM `users` WHERE `uid` = $uid");
+    // $getAccessToken->execute();
+    // while($getAccessTokenNum = $getAccessToken->fetch(PDO::FETCH_ASSOC)) {
+    //     $AccessToken = $getAccessTokenNum['accesstoken_lineoa'];
+    // }
 
     $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
     $lineData['AccessToken'] = "uEbhTcwlpe54y5BHzyjzFpmp8IjkmYvEftYlagXn2HijGkFNv3ONRMVE72iqX5YJETG1T59BEhq4d9T+2x9Vs5QFyLNytZVsV0zbPEvpV51g7H3j7TmJuFTZ1clOB7PlzPTYE/bCXc3a2NNyRC47nAdB04t89/1O/w1cDnyilFU=";
