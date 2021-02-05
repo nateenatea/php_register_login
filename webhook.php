fd9569e288c4c1cd97378380e4c3c4182337fa31
natee <natee.natea@gmail.com>
2021-02-05 21:15:14 +0700

    test

--- webhook.php
+++ webhook.php
@@ -99,9 +99,11 @@
         $AccessToken = $getAccessTokenNum['accesstoken_lineoa'];
     }
 
+    echo $AccessToken;
+
     $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
-    // $lineData['AccessToken'] = "uEbhTcwlpe54y5BHzyjzFpmp8IjkmYvEftYlagXn2HijGkFNv3ONRMVE72iqX5YJETG1T59BEhq4d9T+2x9Vs5QFyLNytZVsV0zbPEvpV51g7H3j7TmJuFTZ1clOB7PlzPTYE/bCXc3a2NNyRC47nAdB04t89/1O/w1cDnyilFU=";
-    $lineData['AccessToken'] = $AccessToken;
+    $lineData['AccessToken'] = "uEbhTcwlpe54y5BHzyjzFpmp8IjkmYvEftYlagXn2HijGkFNv3ONRMVE72iqX5YJETG1T59BEhq4d9T+2x9Vs5QFyLNytZVsV0zbPEvpV51g7H3j7TmJuFTZ1clOB7PlzPTYE/bCXc3a2NNyRC47nAdB04t89/1O/w1cDnyilFU=";
+    // $lineData['AccessToken'] = $AccessToken;
 
     $replyJson["replyToken"] = $replyToken;
     $replyJson["messages"][0] = $replyText;
