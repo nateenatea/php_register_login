c15707f3d19edfc64d0578f73daceaeac7b47a53
Notebook <natee.natea@gmail.com>
2021-02-10 16:12:45 +0700

    test liff

---
+++ liff.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img id="pictureUrl">
+    <p id="userId"><b>userId:</b></p>
+    <p id="statusMessage"><b>statusMessage:</b></p>
+    <p id="displayName"><b>displayName:</b></p>
+    <p id="decodedIDToken"><b>email:</b></p>
+
+    <script src="https://static.line-scdn.net/liff/edge/2.1/liff.js"></script>
+
+    <script>
+
+        async function getUserProfile() {
+            const profile = await liff.getProfile()
+            document.getElementById("pictureUrl").src = profile.pictureUrl
+            document.getElementById("userId").append(profile.userId)
+            document.getElementById("statusMessage").append(profile.statusMessage)
+            document.getElementById("displayName").append(profile.displayName)
+            document.getElementById("decodedIDToken").append(liff.getDecodedIDToken().email)
+            
+        }
+        async function main() {
+            await liff.init({ liffId: "1655607383-lza4vpZb" })
+            if(liff.isLoggedIn()) {
+                getUserProfile()
+            } else {
+                liff.login()
+            }
+        }
+        main()
+    </script>
+</body>
+</html>
