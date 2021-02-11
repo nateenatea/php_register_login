a45b15d31f4291f206fcc49eeddc39fab4dbc00c
Notebook <natee.natea@gmail.com>
2021-02-11 15:34:08 +0700

    test ajax

--- order_home.php
+++ order_home.php
@@ -1,4 +1,5 @@
 <script src="https://static.line-scdn.net/liff/edge/versions/2.5.0/sdk.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script>
 
 async function getUserProfile() {
