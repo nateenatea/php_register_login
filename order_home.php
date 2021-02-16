939a0c2092d77a3be97e265fe64758b196523920
Notebook <natee.natea@gmail.com>
2021-02-16 15:57:58 +0700

    Test notify

--- order_home.php
+++ order_home.php
@@ -67,37 +67,35 @@
 
                     function notify_message($message) {
                         //Get Line Notify Token
+                        $uid = $_GET['u_id'];
                         $getAccessToken = $db->prepare("SELECT * FROM `users` WHERE `uid` = '$uid'");
                         $getAccessToken->execute();
 
                         while($row = $getAccessToken->fetch(PDO::FETCH_ASSOC)) {
                             $AccessToken = $row['accesstoken_notify'];
                         }
-
-                        echo $AccessToken;
                         // $AccessToken = "dKWHuVc0nU8e786i0TP9eWa650ZADeGKlergcwFmQ8K";
-                        // $queryData = array('message' => $message);
-                        // $queryData = http_build_query($queryData,'','&');
-                        // $headerOptions = array(
-                        //     'http' => array(
-                        //         'method' => 'POST',
-                        //         'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
-                        //                     ."Authorization: Bearer ".$AccessToken."\r\n"
-                        //                     ."Content-Length: ".strlen($queryData)."\r\n",
-                        //         'content' => $queryData
-                        //     )
-                        // );
+                        $queryData = array('message' => $message);
+                        $queryData = http_build_query($queryData,'','&');
+                        $headerOptions = array(
+                            'http' => array(
+                                'method' => 'POST',
+                                'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
+                                            ."Authorization: Bearer ".$AccessToken."\r\n"
+                                            ."Content-Length: ".strlen($queryData)."\r\n",
+                                'content' => $queryData
+                            )
+                        );
 
-                        // print_r($headerOptions);
-                        // $context = stream_context_create($headerOptions);
-                        // $result = file_get_contents("https://notify-api.line.me/api/notify", FALSE, $context);
-                        // $res = json_decode($result);
-                        // return $res;
+                        $context = stream_context_create($headerOptions);
+                        $result = file_get_contents("https://notify-api.line.me/api/notify", FALSE, $context);
+                        $res = json_decode($result);
+                        return $res;
                     }
 
                     $res = notify_message($message);
 
-                    // header("refresh:2;order_success.php");
+                    header("refresh:2;order_success.php");
                 }
             }
         } catch (PDOException $e) {
