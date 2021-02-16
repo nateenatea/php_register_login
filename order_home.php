ae5d08b5986193a86025de295c5ed2a598630547
Notebook <natee.natea@gmail.com>
2021-02-16 16:07:04 +0700

    test

--- order_home.php
+++ order_home.php
@@ -67,30 +67,32 @@
 
                     function notify_message($message) {
                         global $uid;
+
+                        echo $uid;
                         //Get Line Notify Token
-                        $getAccessToken = $db->prepare("SELECT * FROM `users` WHERE `uid` = '$uid'");
-                        $getAccessToken->execute();
+                        // $getAccessToken = $db->prepare("SELECT * FROM `users` WHERE `uid` = '$uid'");
+                        // $getAccessToken->execute();
 
-                        while($row = $getAccessToken->fetch(PDO::FETCH_ASSOC)) {
-                            $AccessToken = $row['accesstoken_notify'];
-                        }
-                        // $AccessToken = "dKWHuVc0nU8e786i0TP9eWa650ZADeGKlergcwFmQ8K";
-                        $queryData = array('message' => $message);
-                        $queryData = http_build_query($queryData,'','&');
-                        $headerOptions = array(
-                            'http' => array(
-                                'method' => 'POST',
-                                'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
-                                            ."Authorization: Bearer ".$AccessToken."\r\n"
-                                            ."Content-Length: ".strlen($queryData)."\r\n",
-                                'content' => $queryData
-                            )
-                        );
+                        // while($row = $getAccessToken->fetch(PDO::FETCH_ASSOC)) {
+                        //     $AccessToken = $row['accesstoken_notify'];
+                        // }
+                        // // $AccessToken = "dKWHuVc0nU8e786i0TP9eWa650ZADeGKlergcwFmQ8K";
+                        // $queryData = array('message' => $message);
+                        // $queryData = http_build_query($queryData,'','&');
+                        // $headerOptions = array(
+                        //     'http' => array(
+                        //         'method' => 'POST',
+                        //         'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
+                        //                     ."Authorization: Bearer ".$AccessToken."\r\n"
+                        //                     ."Content-Length: ".strlen($queryData)."\r\n",
+                        //         'content' => $queryData
+                        //     )
+                        // );
 
-                        $context = stream_context_create($headerOptions);
-                        $result = file_get_contents("https://notify-api.line.me/api/notify", FALSE, $context);
-                        $res = json_decode($result);
-                        return $res;
+                        // $context = stream_context_create($headerOptions);
+                        // $result = file_get_contents("https://notify-api.line.me/api/notify", FALSE, $context);
+                        // $res = json_decode($result);
+                        // return $res;
                     }
 
                     $res = notify_message($message);
